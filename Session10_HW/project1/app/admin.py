ff741272963f8d21f832264d273c6f13b1483a2c
kchaehyeon01 <kchaehyeon01@gmail.com>
2022-05-19 17:09:01 +0900

    0519

---
+++ Session10_HW/project1/app/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Post, Comment
+
+# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Comment)
+
+
+
+
